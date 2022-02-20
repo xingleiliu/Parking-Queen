fcfb0d95818dd8c9d3f602653921e3d50f1e3fba
Xinglei Liu <liu2177@purdue.edu>
2022-02-20 00:52:10 -0800

    add scripts and data file

---
+++ api.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import folium
+from streamlit_folium import folium_static
+import requests
+import urllib.parse
+import pandas as pd
+from map_format import add_legends_popup
+from core_data import get_data
+
+st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+st.header("Parking Queen")
+st.subheader("Find Meter Parking in Los Angeles")
+
+location = st.text_input("Enter Your Destination: ")
+
+time = st.select_slider(
+     'Select Time You Want to Park: ',
+     options=['1 hour', '2 hours', '3 hours', '4+ hours'])
+
+
+radius = st.slider('Select Radius (miles): ', 0.0, 5.0, 1.0)
+
+space_type = st.radio(
+     "Select Space Type",
+     ('Single-space', 'Multi-space'))
+
+
+def empty_map():
+    folium_map = folium.Map(location=[34.0522, -118.2437], zoom_start=12)
+    return folium_map
+
+
+if st.button('Find Parking Space'):
+    data, lat, long = get_data(location, radius)
+    # print(len(data))
+    if 3 <= radius <= 5:
+        zoom_start = 12
+    else:
+        zoom_start = 15
+    folium_map = add_legends_popup(data, folium.Map(zoom_start=zoom_start, location=[lat, long]))
+else:
+     folium_map = empty_map()
+
+
+# folium_map = generate_map("700 W 9th St")
+folium_static(folium_map)
---
+++ map_format.py
@@ -0,0 +1,110 @@
+
+import pandas as pd
+import folium
+import branca
+import numpy as np
+
+
+
+def add_legends_popup(dataframe, m):
+    
+    lgd_txt = '<span style="color: {col};">{txt}</span>'
+    price_list = ['less or equal to $1', 'between $1 and $2', 'between $2 and $3', 'greater than $3']
+    color = ['green', 'blue', 'orange', 'red']
+
+    for idx in range(len(price_list)): 
+
+        fg = folium.FeatureGroup(name= lgd_txt.format( txt= 'Lowest Rate: '+ price_list[idx] , col= color[idx]))
+        m.add_child(fg)
+        
+    for i in range(0,len(dataframe)):
+        LowestRate = dataframe['LowestRate'].iloc[i]
+        if LowestRate <= np.float(1.0):
+            color = 'green'
+        elif LowestRate > np.float(1.0) and LowestRate <= np.float(2.0):
+            color = 'blue'
+        elif LowestRate > np.float(2.0) and LowestRate <= np.float(3.0):
+            color = 'orange'
+        else:
+            color = 'red'
+
+        html = popup_html(i, dataframe)
+        iframe = branca.element.IFrame(html=html,width=510,height=280)
+        popup = folium.Popup(folium.Html(html, script=True), max_width=500)
+
+
+        folium.Marker([dataframe['Lan'].iloc[i],dataframe['Long'].iloc[i]],popup=popup,
+                     icon=folium.Icon(color=color, prefix='fa')).add_to(m) 
+
+    folium.map.LayerControl('bottomleft', collapsed= False).add_to(m)
+
+    
+    return m 
+
+
+
+
+
+
+def popup_html(row, dataframe):
+    
+    i = row
+    BlockFace=dataframe['BlockFace'].iloc[i] 
+    MeterType=dataframe['MeterType'].iloc[i]
+    RateType = dataframe['RateType'].iloc[i] 
+    RateRange=dataframe['RateRange'].iloc[i] 
+    MeteredTimeLimit = dataframe['MeteredTimeLimit'].iloc[i]                      
+    ParkingPolicy = dataframe['ParkingPolicy'].iloc[i]
+
+    left_col_color = "#B4CFB0"
+    right_col_color = "#F1E0AC"
+    
+    html = """<!DOCTYPE html>
+<html>
+
+<head>
+<h4 style="margin-bottom:10"; width="200px">{}</h4>""".format(BlockFace) + """
+
+</head>
+    <table style="height: 126px; width: 350px;">
+<tbody>
+<tr>
+<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Meter Type</span></td>
+<td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(MeterType) + """
+</tr>
+<tr>
+<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Rate Type</span></td>
+<td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(RateType) + """
+</tr>
+<tr>
+<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Rate Range</span></td>
+<td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(RateRange) + """
+</tr>
+<tr>
+<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Metered Time Limit</span></td>
+<td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(MeteredTimeLimit) + """
+</tr>
+<tr>
+<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Parking Policy</span></td>
+<td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(ParkingPolicy) + """
+</tr>
+</tbody>
+</table>
+</html>
+"""
+    return html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
